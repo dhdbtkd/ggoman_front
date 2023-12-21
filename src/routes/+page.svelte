685f372d28cafc763ed1597d5899f0bcc1b3cca4
yoosangoh <dhdbtkd@naver.com>
2023-12-22 01:09:14 +0900

    socket 주소 변경

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,12 +79,12 @@
             console.log("i'am alive");
             socket.emit("i_am_alive", name);
         },10000)
-        // socket = io("wss://port-0-ggoman-back-koh2xlivr60m6.sel4.cloudtype.app",{
-        //     timeout : 50000
-        // })
-        socket = io("ws://192.168.50.31:3000/",{
+        socket = io("wss://port-0-ggoman-back-koh2xlivr60m6.sel4.cloudtype.app",{
             timeout : 50000
         })
+        // socket = io("ws://192.168.50.31:3000/",{
+        //     timeout : 50000
+        // })
 
         socket.on("connect", () => {
             if (socket.recovered) {
